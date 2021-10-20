9d9c534054910e41529edcc4fe4af01c7cb7667f
ctrl-alt-delor <none@none>
2021-10-20 18:38:52 +0100

    add error message

--- multi-copy/muli-copy.py
+++ multi-copy/muli-copy.py
@@ -5,12 +5,18 @@ import shutil
 from os import listdir
 from os.path import isfile, isdir, join
 
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 if len(sys.argv) == 3 :
     to_path=sys.argv[1]
     file=sys.argv[2]
 else:
-    to_path="a"
-    file="test"
+    self_name=sys.argv[0]
+    eprint(f"error: Usage {self_name} to_path from_file")
+    eprint(f"where to_path is a directory containing all the directories that you want the file in")
+    eprint(f"and from_file is a file or directory")
+    exit(-1)
 
 def do_copy(to_path,file):
     sub_dirs = [ dir+ "/" for dir in
